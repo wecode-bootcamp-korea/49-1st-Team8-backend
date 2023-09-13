0e4e6266db15adeff9ef21b51efbdf46f457c33c
namsuhan0322 <rnatpakfl@gmail.com>
2023-09-13 20:34:11 +0900

    push

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+require("dotenv").config();
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", userService.hello);
+app.get("/ping", async (req, res) => {
+  try {
+    return res.status(200).json({ message: "pong" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const server = http.createServer(app);
+
+const start = async () => {
+  try {
+    server.listen(8000, () => console.log(`Server is listening on 8000`));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
