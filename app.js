8e50fb4f2d6b4e3f35417f721017c53f3171af61
imjeongjin <kjj_010101@naver.com>
2023-09-13 18:10:07 +0900

    [feature] 필수 구현 사항 완료

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-require("dotenv").config();
-const app = express();
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const userService = require('./services/userService.js')
+const threadService = require("./services/threadService.js")
 
-dotenv.config();
-app.use(express.json());
+//app.js에서 express 함수 사용
+const app = express()
+
+//프론트랑 연결하기 위해 필요
 app.use(cors());
 
-app.get("/", userService.hello);
-app.get("/ping", async (req, res) => {
-  try {
-    return res.status(200).json({ message: "pong" });
-  } catch (error) {
-    console.log(error);
-  }
-});
+//express 서버를 json 형식으로 사용
+app.use(express.json())
 
-const server = http.createServer(app);
 
-const start = async () => {
-  try {
-    server.listen(8000, () => console.log(`Server is listening on 8000`));
-  } catch (err) {
-    console.error(err);
-  }
-};
+const linkServer = async(req, res) => {
+    try{
+    return res.status(200).json({"message" : "Welcome to Jeongjin's Server!"})
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-start();
+//url 입력 안한 기본 서버
+app.get('/', linkServer)
+
+//회원가입 API
+app.post("/users/signup", userService.userSignup)
+
+//로그인 API
+app.post("/users/login", userService.userLogin)
+
+/* -------------------게시글 시작------------------------ */
+
+//게시글 조회
+app.get('/posts/read', threadService.readThreads)
+
+// //게시글 작성
+app.post('/posts/create', threadService.createThreads)
+
+//서버 시작에 필요
+const server = http.createServer(app)
+
+const start = async() => {
+    try {
+        server.listen(8001, () => console.log(`Server is listening on 8001`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
